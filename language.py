cc0ca1a81d16ac9b2ba3a49ffff6bc06473812ed
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2021-11-01 12:36:28 +0530

    Compute-Uniform-Probabilities

--- language.py
+++ language.py
@@ -138,7 +138,14 @@ Parameters: list of strs
 Returns: list of floats
 '''
 def buildUniformProbs(unigrams):
-    return
+    dict_={}
+    temp_list=[]
+    for i  in unigrams:
+        if i not in dict_:
+            dict_[i]=1
+    for x,y in dict_.items():
+        temp_list.append(y/len(dict_))
+    return temp_list
 
 
 '''
